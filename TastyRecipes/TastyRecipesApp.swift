d19518b18d14b43b4c74a62c02b19d0d0af2fd69
vdotup <v.up@live.com>
2025-02-03 18:37:26 +0300

    Initial Commit

---
+++ TastyRecipes/TastyRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyRecipesApp.swift
+//  TastyRecipes
+//
+//  Created by vdotup on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TastyRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
